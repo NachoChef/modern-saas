99566216d2c43bfa0a7bf5f73e9077e2f6aa97df
Justin Jones <hock3ydud3@gmail.com>
2023-07-12 21:48:14 -0500

    module 3/account stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,41 @@
 <script lang="ts">
 	import "../app.css";
 	import { page } from "$app/stores";
-	import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, Button } from "flowbite-svelte";
+	import {
+		Navbar,
+		NavBrand,
+		NavHamburger,
+		NavUl,
+		NavLi,
+		Button,
+		Dropdown,
+		Chevron,
+		DropdownItem
+	} from "flowbite-svelte";
 	import { invalidate } from "$app/navigation";
 	import type { LayoutData } from "./$types";
-	import { onMount } from "svelte"
+	import { onMount } from "svelte";
 
 	const navigation = [
 		{ label: "Home", href: "/" },
 		{ label: "Pricing", href: "/pricing" },
-		{ label: "Contacts", href: "/contacts" },
-		{ label: "Account", href: "/account" }
+		{ label: "Contacts", href: "/contacts" }
 	];
 
 	export let data: LayoutData;
 
 	// pull session/supabase from data
-	$: ({session, supabase} = data)
+	$: ({ session, supabase } = data);
 
 	onMount(() => {
 		// subscribe to supabase auth state change
-		const {data: {subscription}} = supabase.auth.onAuthStateChange((event, _session) => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange((event, _session) => {
 			// if not the same then session isn't the same, invalidate the supabase:auth id
 			// depending on supabase:auth id will trigger re-retrieving the session?
-			if(_session?.expires_at !== session?.expires_at) {
-				invalidate("supabase:auth")
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate("supabase:auth");
 			}
 		});
 
@@ -45,13 +56,27 @@
 				Contactly
 			</span>
 		</NavBrand>
-		<div class="flex md:order-2">
-			<div class="flex items-center gap-2">
-				<Button href="/login" size="sm">Login</Button>
-				<Button href="/register" size="sm" color="alternative">Register</Button>
+		{#if session}
+			<Button color="light"><Chevron>Account</Chevron></Button>
+			<Dropdown>
+				<div slot="header" class="px-4 py-2">
+					<span class="block truncate text-sm font-medium"> {session.user.email} </span>
+				</div>
+				<DropdownItem href="/account">Settings</DropdownItem>
+				<DropdownItem href="/account">Billing</DropdownItem>
+				<form action="/logout" method="POST">
+					<DropdownItem type="submit" slot="footer">Sign out</DropdownItem>
+				</form>
+			</Dropdown>
+		{:else}
+			<div class="flex md:order-2">
+				<div class="flex items-center gap-2">
+					<Button href="/login" size="sm">Login</Button>
+					<Button href="/register" size="sm" color="alternative">Register</Button>
+				</div>
 			</div>
-			<NavHamburger on:click={toggle} />
-		</div>
+		{/if}
+		<NavHamburger on:click={toggle} />
 		<NavUl {hidden}>
 			{#each navigation as nav}
 				<NavLi href={nav.href} active={$page.url.pathname === nav.href}>{nav.label}</NavLi>
--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,21 +1,7 @@
 import { fail, type Actions, redirect } from '@sveltejs/kit';
 import { setError, superValidate } from 'sveltekit-superforms/server';
-import { z } from "zod";
 import type { PageServerLoad } from './$types';
-
-const registerUserSchema = z.object({
-    // nullish => optional
-  full_name: z.string().max(140, "Name must be 140 characters or less").nullish(),
-  email: z.string().email("Invalid email address"),
-  password: z
-    .string()
-    .min(8, "Password must be at least 8 characters")
-    .max(64, "Password must be 64 characters or less"),
-  passwordConfirm: z
-    .string()
-    .min(8, "Password must be at least 8 characters")
-    .max(64, "Password must be 64 characters or less"),
-});
+import { registerUserSchema } from '$lib/schemas';
 
 export const load = (async (event) => {
     const session = await event.locals.getSession();
